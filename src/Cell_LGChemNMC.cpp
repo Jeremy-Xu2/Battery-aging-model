87d561ea6d9d5b845d0b56ea6fa25791039224c9
Jeremy Xu <jxu@abslocal.com>
2021-09-13 22:00:24 -0400

    LG normal trial 8

--- src/Cell_LGChemNMC.cpp
+++ src/Cell_LGChemNMC.cpp
@@ -216,7 +216,7 @@ Cell_LGChemNMC::Cell_LGChemNMC(const Model& MM, int verbosei){
 		seiparam.sei1k = 0.15*pow(10,-13);
 		seiparam.sei1k_T = 13000;
 		seiparam.sei2k = 4.5*pow(10,-12);
-		seiparam.sei2k_T = 25000;
+		seiparam.sei2k_T = 15000;
 		seiparam.sei2D = 1*pow(10,-14);
 		seiparam.sei2D_T = 100000;
 		seiparam.sei3k = pow(10,-11);
